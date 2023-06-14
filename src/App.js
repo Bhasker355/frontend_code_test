feee6e1d203e5881868ca17f663fe86bc29bf85e
Bhasker355 <veerla.bhasker.s@gmail.com>
2023-06-14 22:35:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="https://bhasker355.github.io/frontend_code_test/"
-            element={<Home />}
-          />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Home />
     </>
   );
 }
